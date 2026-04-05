91fb83230b5a0e125c683849f1255bec77d144d7
yashok87 <yashok@gmail.com>
2026-04-05 16:07:08 +0100

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@
 // @ts-nocheck
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   base: '/', 
   define: {
     // Safely inject the API keys.
